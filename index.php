6b1f10379980840443ba7fd8f1f9c3d7f8fe09d4
Febri Milansyah <febrimilan00@gmail.com>
2023-03-20 22:56:43 +0700

    2023-03-16T22:56:12

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?= 'Hei' ?>
+<?php
+$data = [
+    [
+        'illustration' => 'https://i.imgur.com/UYiroysl.jpg',
+    ],
+    [
+        'illustration' => 'https://i.imgur.com/UPrs1EWl.jpg',
+    ],
+    [
+        'illustration' => 'https://i.imgur.com/MABUbpDl.jpg',
+    ],
+    [
+        'illustration' => 'https://i.imgur.com/KZsmUi2l.jpg',
+    ],
+    [
+        'illustration' => 'https://i.imgur.com/2nCt3Sbl.jpg',
+    ],
+    [
+        'illustration' => 'https://i.imgur.com/lceHsT6l.jpg',
+    ],
+];
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($data);
